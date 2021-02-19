37bc14273ca4163922dcc22535299cb6900e805b
Amr Abdelfatah <prog.amr2014@gmail.com>
2021-02-20 01:54:11 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CSS for backend devs</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:400,700"/>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/app.css" />
+        <link rel="stylesheet" href="css/normalize.css" />
+    </head>
+    <body>
+        <header class="section">
+            <div class="container">
+                <div class="header-top">
+                    <h1>XCasts</h1>
+                    <a href="#">Sign In</a>
+                </div>
+                <div>
+                    <nav>
+                        <a href="#">Catalog</a>
+                        <a href="#">Series</a>
+                        <a href="#">Podcast</a>
+                        <a href="#">Discussions</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="box">
+                            Some Callout Text
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="box">
+                            Some Callout Text
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="box">
+                            Some Callout Text
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="box">
+                            Some Callout Text
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
