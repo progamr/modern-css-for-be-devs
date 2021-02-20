838533aa0765dee5547dca3218ad1e823ca6df3e
Amr Abdelfatah <prog.amr2014@gmail.com>
2021-02-20 04:47:44 +0200

    Series Card UI design

---
+++ resources/views/card.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSS for backend devs</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:400,700"/>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/card.css" />
+    <link rel="stylesheet" href="css/normalize.css" />
+</head>
+<body>
+    <div class="card">
+        <!--left-->
+        <div class="card-left">
+            <a href="#" class="card-skill-button">PHP</a>
+
+            <img src="testing-jargon-2.png" alt="" class="series-icon">
+
+            <span class="card-difficulty">Intermediate Difficulty</span>
+        </div>
+        <!--right-->
+        <div class="card-right">
+            <h3 class="card-title"><a href="#">Testing Jargon</a></h3>
+            <p class="card-excerpt">
+                There's no two ways about it: terminology in the testing world is incredibly overwhelming. Mocks, stubs, and dummies oh my!
+            </p>
+            <div class="card-meta">
+                <div>
+                    <img src="book.svg" alt="" class="meta-icon">
+                    5 Lessons
+                </div>
+                <div>
+                    <img src="clock.svg" alt="" class="meta-icon">
+                    53:10 min
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/card', function () {
+    return view('card');
+});
